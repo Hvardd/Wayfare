26947a73cc97b923684236383d759f8c7bedc1eb
Hvardd <37109668+Hvardd@users.noreply.github.com>
2019-04-05 12:40:23 +0200

    Add files via upload
    Implementer fyll inn, og fyll insert inn i hovednettsiden, fjernet tom mappe fra github, endret på sql connect til databasen. 
    Lagt til header og footer i include på liste.php listeinput.php, lagt til header() på php Fyllliste.php insert.php

--- src/listeinput.php
+++ src/listeinput.php
@@ -16,7 +16,7 @@
 
     <!-- Navigasjons bar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#"><a href="/"><h1 class="logo">Wayfare</h1></a> <!-- Wayfare logo -->
+            <a class="navbar-brand" href="#"><a href="index.php"><h1 class="logo">Wayfare</h1></a> <!-- Wayfare logo -->
     
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
@@ -25,10 +25,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Hjem</a>
+                        <a class="nav-link" href="index.php">Hjem</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="profil.html">Profil</a>
+                        <a class="nav-link" href="profil.php">Profil</a>
                     </li>
     
                     <li class="nav-item dropdown">
@@ -43,9 +43,10 @@
                         </div>
                     </li>
     
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Registrer / Login</a>
-                    </li>
+                   <?php 
+                    include_once "include/navcheck.php";
+                    ?>         
+                    
                     <li class="nav-item">
                         <a class="nav-link" href="liste.php">Kjøreliste</a>
                     </li>
