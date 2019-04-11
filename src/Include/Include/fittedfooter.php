f19a049d7d679f328b246524262760d60dd27116
ThomasUSN <35728707+ThomasUSN@users.noreply.github.com>
2019-04-11 17:04:26 +0200

    php til footer som ikke tar ekstra plass

---
+++ src/Include/Include/fittedfooter.php
@@ -0,0 +1,107 @@
+
+<!-- Footer -->
+<footer class="footerHover page-footer font-small stylish-color-dark pt-4 bg-primary">
+
+        <!-- Footer Links -->
+        <div class="footerHover container text-center text-md-left text-white">
+    
+          <!-- Grid row -->
+          <div class="footerHover row">
+    
+            <!-- Grid column -->
+            <div class="col-md-4 mx-auto">
+    
+              <!-- Content -->
+              <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Her finner du noen linker</h5>
+              <p>Her kan du finne ulike linker til steder på nettsiden, eller så kan du trykke på "Tilbake til toppen" knappen for å komme tilbake til toppen av nettsiden.</p>
+    
+            </div>
+            <!-- Grid column -->
+    
+            <hr class="clearfix w-100 d-md-none">
+    
+            <!-- Grid column -->
+            <div class="col-md-2 mx-auto">
+    
+              <!-- Links -->
+              <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Linker</h5>
+    
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#!">Hjem</a>
+                </li>
+                <li>
+                  <a href="#!">Hvordan bruke Wayfare?</a>
+                </li>
+                <li>
+                  <a href="#!">Spørsmål og svar</a>
+                </li>
+                <li>
+                  <a href="#!">Informasjon om Wayfare</a>
+                </li>
+              </ul>
+    
+            </div>
+            <!-- Grid column -->
+    
+            <hr class="clearfix w-100 d-md-none">
+    
+            <!-- Grid column -->
+            <div class="col-md-2 mx-auto">
+    
+              <!-- Links -->
+              <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Linker</h5>
+    
+              <ul class="list-unstyled">
+                <li>
+                  <a href="profil.html">Din profil</a>
+                </li>
+                <li>
+                  <a href="#!">Bytte bruker</a>
+                </li>
+                <li>
+                  <a href="#!">Bruker oversikt</a>
+                </li>
+                <li>
+                  <a href="#!">Logg ut</a>
+                </li>
+              </ul>
+    
+            </div>
+            <!-- Grid column -->
+    
+            <hr class="clearfix w-100 d-md-none">
+            <!-- Grid column -->
+    
+          </div>
+          <!-- Grid row -->
+    
+        </div>
+        <!-- Footer Links -->
+    
+        <hr>
+    
+        <ul class="list-unstyled list-inline text-center py-2">
+          <li class="list-inline-item">
+            <h5 class="mb-1"></h5>
+          </li>
+          <li class="list-inline-item">
+            <a href="#Navbar" class="btn btn-danger btn-rounded" onclick="topFunction()" id="tilbakeKnapp" >Tilbake til toppen</a>
+          </li>
+        </ul>
+    
+        <hr>
+    
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3 text-white">© 2019 Copyright:
+          <a href="index.html"> Wayfare.no</a>
+        </div>
+        <!-- Copyright -->
+    
+    </footer>
+    <!-- Footer -->
+
+  </body>
+
+
+</html>
